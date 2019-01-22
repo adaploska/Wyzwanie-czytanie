932579f341891449ab045c9121388ae088a076bd
adaploska <adawagrocka@wp.pl>
2019-01-22 13:22:37 +0100

    add media query to progress bar

--- src/components/textImageComponent.js
+++ src/components/textImageComponent.js
@@ -3,7 +3,7 @@ import React from "react"
 class TextImageComponent extends React.Component {
     render() {
         return (
-            <div>
+            <div className="p-3 my-4 text-center pt-5 ">
                 <h1>Wyzwanie - Czytanie!</h1>
                 <img alt="book" src="/ksiazka.svg" width="150"></img>
             </div>
